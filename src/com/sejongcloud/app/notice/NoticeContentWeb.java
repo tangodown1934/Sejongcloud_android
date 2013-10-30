3519858bad475b854de2523f1a6ea7679d905cc2
tangodown1934 <tangodown1934@gmail.com>
2013-10-30 16:53:29 +0900

    resizing splash image, publised

---
+++ src/com/sejongcloud/app/notice/NoticeContentWeb.java
@@ -0,0 +1,55 @@
+package com.sejongcloud.app.notice;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.sejongcloud.app.R;
+import com.sejongcloud.app.dialog.TransDialog;
+
+public class NoticeContentWeb extends Activity {
+	WebView mWebView;
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.notice_content_web);
+
+		TransDialog.showLoading(this);
+
+		String url = getIntent().getStringExtra("url");
+		mWebView = (WebView) findViewById(R.id.noticeWebView);
+		mWebView.setVisibility(View.INVISIBLE);
+		mWebView.setWebViewClient(new goNoticeClient()); // WebViewClient ÁöÁ¤
+		mWebView.getSettings().setJavaScriptEnabled(true);
+		mWebView.loadUrl(url);
+		mWebView.setWebChromeClient(new WebChromeClient() {
+			public void onProgressChanged(WebView view, int progress) {
+				if (progress == 100) {
+					mWebView.setVisibility(View.VISIBLE);
+					TransDialog.hideLoading();
+				} else {
+					TransDialog.showLoading(NoticeContentWeb.this);
+				}
+			}
+		});
+	}
+
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+			mWebView.goBack();
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+	private class goNoticeClient extends WebViewClient {
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			view.loadUrl(url);
+			return true;
+		}
+	}
+}
---
+++ src/com/sejongcloud/app/tip/Tip.java
@@ -0,0 +1,60 @@
+package com.sejongcloud.app.tip;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.sejongcloud.app.R;
+
+public class Tip extends Activity {
+	WebView mWebView;
+	ProgressDialog dialog;
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tip);
+
+		dialog = new ProgressDialog(this);
+		dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+		dialog.setCancelable(false);
+		dialog.show();
+
+		mWebView = (WebView) findViewById(R.id.TipWebView);
+		mWebView.setVisibility(View.INVISIBLE);
+		mWebView.setWebViewClient(new webClient()); // WebViewClient ÁöÁ¤
+		mWebView.getSettings().setJavaScriptEnabled(true);
+		mWebView.setWebChromeClient(new WebChromeClient() {
+			public void onProgressChanged(WebView view, int progress) {
+				dialog.setProgress(progress);
+
+				if (progress == 100) {
+					mWebView.setVisibility(View.VISIBLE);
+					dialog.dismiss();
+				} else {
+					dialog.show();
+				}
+			}
+		});
+		mWebView.loadUrl("http://sejongcloud.com");
+	}
+
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+			mWebView.goBack();
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+	private class webClient extends WebViewClient {
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			view.loadUrl(url);
+			return true;
+		}
+	}
+}
